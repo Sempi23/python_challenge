5ad2a6a78747d522f1fd9e56b210b75d904357b7
Lynda <lsempele@yahoo.com>
2024-03-22 15:32:33 -0400

    final update

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,16 +2,16 @@ import os
 import csv
 
 # path to collect csv data from the Resources folder
-#budget_data = os.path.join('Resources', 'budget_data.csv')
+budget_data = os.path.join('Resources', 'budget_data.csv')
 
-file_to_load = "PyBank/Resources/budget_data.csv"
+# OR file_to_load = "PyBank/Resources/budget_data.csv"
 
 # Output path
-file_to_output = "PyBank/analysis.budget_analysis.txt"
+file_to_output = "Analysis/budget_analysis.txt"
 
 # Open and read the csv file
-#with open(budget_data, newline="") as csvfile:
-with open(file_to_load) as csvfile:
+with open(budget_data, newline="") as csvfile:
+#with open(file_to_load) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
 # skip header row 
@@ -24,6 +24,7 @@ with open(file_to_load) as csvfile:
     average_change = 0
     previous_profit_loss = 0
     monthly_changes = []
+    monthly_dates = []
     greatest_increase = 0
     greatest_decrease = 0
     greatest_decrease_date = []
@@ -39,16 +40,19 @@ with open(file_to_load) as csvfile:
         net_total += int(row[1])
 
     # calculate changes in profit/losses over the entire period
-    if previous_profit_loss != 0:
         monthly_change = int(row[1]) - previous_profit_loss
         monthly_changes.append(monthly_change)
+        monthly_dates.append(row[0])
 
     # update previous_profit_loss for the next iteration
         previous_profit_loss = int(row[1])
-
+#print(monthly_changes)
+        
 # calculate the average profit/loss change
 if len(monthly_changes) > 0:  
-    average_change = sum(monthly_changes) / len(monthly_changes)
+    # exclude the first month when calculating average change because it doesn't have a previous month to calculate change from
+    average_change = sum(monthly_changes[1:]) / len(monthly_changes[1:]) 
+    average_change = round(average_change, 2) #round to two decimal places
 
 # calculate greatest increase in profit/loss(date & amount)  over the entire period
     greatest_increase = max(monthly_changes)
@@ -57,20 +61,27 @@ if len(monthly_changes) > 0:
     greatest_decrease = min(monthly_changes)
 
 # find corresponding date for the greatest increase and decrease
-    greatest_increase_date = row[monthly_changes.index(greatest_increase)]
-    greatest_decrease_date = row[monthly_changes.index(greatest_decrease)]
+    greatest_increase_date = monthly_dates[monthly_changes.index(greatest_increase)]
+    greatest_decrease_date = monthly_dates[monthly_changes.index(greatest_decrease)]
     
 # Print Analysis results
 
 print("Financial Analysis")
 print("-----------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total: {net_total}")
-print(f"Average Change: {average_change}")
-print(f"Greatest Increase in Profits: {greatest_increase_date[0]} (${greatest_increase}")
-print(f"Greatest Decrease in Profits: {greatest_decrease_date[0]} (${greatest_decrease}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
 # write analysis results to a text file 
-#with open(file_to_output, "w") as txtfile:
-    #txtfile.write(output)
+with open(file_to_output, "w") as txtfile:
+        
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("-----------------------------\n")
+    txtfile.write(f"Total Months: {total_months}\n")
+    txtfile.write(f"Total: ${net_total}\n")
+    txtfile.write(f"Average Change: ${average_change}\n")
+    txtfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
+    txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
 
--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -2,28 +2,22 @@ import os
 import csv
 
 # path to collect data from the Resources Folder
-#election_data = os.path.join("Resources", "election_data.csv")
-file_to_load = "PyPoll/Resources/election_data.csv"
+election_data = os.path.join("Resources", "election_data.csv")
 
 # Output file
-file_to_output = "PyPoll/analysis/election-analysis_txt"
+file_to_output = "Analysis/election-analysis_txt"
 
 # Open and read csv file
-#with open(election_data, 'r') as csvfile:
-with open(file_to_load) as csvfile:
+with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-# skip header row
-    csv_header = next(csvreader) 
+    csv_header = next(csvreader) # skip header row
     
 # Declare variables
     total_votes = 0
-    winner_count = 0
-    Stockham_votes = 0
-    DeGette_votes = 0
-    Doane_votes = 0
-    candidates = []
+    candidate_list = []
     votes = []
-    candidate_votes = {} # dictionary to store votes for each candidate
+    candidate_and_votes = {} # dictionary to store votes for each candidate
+    percentages = {} # dictionary to store percentage of votes for each candidate
     
 
 # Find the total number of votes cast
@@ -31,44 +25,46 @@ with open(file_to_load) as csvfile:
         total_votes += 1 
 
     #collect candidates and their votes
-    candidate = row[2]
-
-    if row[2] == "Charles_Casper_Stockham":
-            Stockham_votes +=1
-    elif row[2] == "Diana_DeGette":
-            DeGette_votes +=1
-    elif row[2] == "Raymon_Anthony_Doane":
-            Doane_votes +=1
-
-    
-
-# Calculate percentage of votes each cadidate won
-    #for candidate, votes in candidate_votes.items():
-        #percent = (votes /total_votes) * 100.0
-        #print(f"{candidate} Percentage: {percent}")
-#Stockham_percent = (Stockham_votes / total_votes) * 100
-#DeGette_percent = (DeGette_votes / total_votes) * 100
-#Doane_percent = (Doane_votes / total_votes) * 100
-
-#print(f"Stockham Percentage: {Stockham_percent}")
-#print(f"DeGette Percentage: {DeGette_percent}")
-#print(f"Doane Percentage: {Doane_percent}")           
-    
-# Calculate the total number of votes each candidate won
-    #make a dictionary out of the two lists
-       # candidates = ["Stockhom", "DeGette", "Doane"]
-        #votes = ["stochom_votes", "DeGette_votes", "Doane_votes"]
-    # Zip the two lists together with candidate as the key and total_votes as the value
-            #dict_candidates_and_votes =dict(zip(candidates, votes))
-            #key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
-    
-# Determine the winner of the election based on popular vote
+        candidate = row[2]
+        if candidate not in candidate_and_votes:
+                candidate_and_votes[candidate] = 0
+        candidate_and_votes[candidate] += 1
         
-# Analysis Results
+#calculate the percentage of votes each candidate won
+    for candidate,votes  in candidate_and_votes.items():
+        percent_votes = (votes / total_votes)  * 100
+        percentages[candidate] = (candidate_and_votes[candidate] / total_votes) * 100
+        #print(f"{candidate} percentage: {percent_votes:.3f}%")
+
+# Determine the winner of the election based on popular vote
+        winner = max(candidate_and_votes, key=candidate_and_votes.get)
+    #print(f"winner: {winner}")
+        
+# Analysis summary table
 print(f"Election Results\n")
 print(f"--------------------------\n")
 print(f"Total Votes: {total_votes}\n")
 print(f"--------------------------\n")
-print(f"Charles Casper Stockham: {Stockham_votes}")
+for candidate, votes in candidate_and_votes.items():
+    print(f"{candidate}:  {percentages[candidate]:.3f}% ({votes})")
+
+print(f"--------------------------\n")
+print(f"Winner: {winner}\n")
+print(f"--------------------------\n")
+
+# write output to analysis folder
+with open(file_to_output, "w") as txtfile:
+        
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"--------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write(f"--------------------------\n")
+    for candidate, votes in candidate_and_votes.items():
+            txtfile.write(f"{candidate}: {percentages[candidate]:.3f}%({votes})\n")
+
+    txtfile.write(f"--------------------------\n")
+    txtfile.write(f"Winner: {winner}")
+    txtfile.write(f"--------------------------\n")
+
 
 
